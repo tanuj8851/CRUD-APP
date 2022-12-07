8034c4242b4a3524c36b1da184b94868095501ee
tanuj8851 <tanujmaurya40@gmail.com>
2022-12-07 14:41:11 +0530

    Done

--- unit-2 A-Sync/sprint-3/day-4/assignments/masai-ops-343-531-KPpjgd-345578aa7e5abf350c220a76737fe258e553adb1/Scripts/index.js
+++ unit-2 A-Sync/sprint-3/day-4/assignments/masai-ops-343-531-KPpjgd-345578aa7e5abf350c220a76737fe258e553adb1/Scripts/index.js
@@ -1,14 +1,17 @@
 // Write code related to Home page here 
-let form = document.getElementById("form");
-let data = JSON.parse(localStorage.getItem("task-list")) || [];
-form.addEventListener("submit", (el) => {
-    let info = {
-        name: form.name.value,
-        desc: form.desc.value,
-        date1: form.start.value,
-        date2: form.end.value,
-        priority: form.priority.value
-    }
-    data.push(info);
-    localStorage.setItem("task-list", JSON.stringify(data));
-})
+let userData=JSON.parse(localStorage.getItem("task-list"))||[]
+let  userForm=document.getElementById("user_form")
+userForm.addEventListener("submit",submitUser)
+function submitUser(e){
+e.preventDefault();
+ const name=document.getElementById("name").value;
+ const description=document.getElementById("desc").value;
+ const startDate=document.getElementById("start").value;
+ const endDate=document.getElementById("end").value;
+ const priority=document.getElementById("priority").value
+ const user={name,description,startDate,endDate,priority}
+  userData.push(user)
+  localStorage.setItem("task-list",JSON.stringify(userData))
+
+
+}
