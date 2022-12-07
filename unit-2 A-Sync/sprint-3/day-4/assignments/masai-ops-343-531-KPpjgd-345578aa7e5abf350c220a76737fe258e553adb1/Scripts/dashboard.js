3e7735cd36b2a521d54e6423f2c1f2c115cb7220
tanuj8851 <tanujmaurya40@gmail.com>
2022-12-07 14:34:50 +0530

    Local Done

--- unit-2 A-Sync/sprint-3/day-4/assignments/masai-ops-343-531-KPpjgd-345578aa7e5abf350c220a76737fe258e553adb1/Scripts/dashboard.js
+++ unit-2 A-Sync/sprint-3/day-4/assignments/masai-ops-343-531-KPpjgd-345578aa7e5abf350c220a76737fe258e553adb1/Scripts/dashboard.js
@@ -1,36 +1,63 @@
 // Write code related to dashboard page here
-let getData = JSON.parse(localStorage.getItem("task-list"));
+let userData = JSON.parse(localStorage.getItem("task-list")) || []
+let priority_list = JSON.parse(localStorage.getItem("priority-list")) || []
 let tbody = document.querySelector("tbody");
+let task_count = document.getElementById("task-count")
+let dofilter = document.getElementById("filter")
+dofilter.addEventListener("change", filterby)
 
-let count = document.getElementById("task-count");
+function filterby() {
+    let value = dofilter.value;
+    userData = JSON.parse(localStorage.getItem("task-list"))
+    if (value == "Low") {
+        userData = userData.filter((user) => user.priority == "Low")
+        showUser()
+    } else if (value == "High") {
+        userData = userData.filter((user) => user.priority == "High")
+        showUser()
+    } else if (value == "Medium") {
+        userData = userData.filter((user) => user.priority == "Medium")
+        showUser()
+    } else {
+        userData = JSON.parse(localStorage.getItem("task-list"))
+        showUser()
+    }
 
-
-function showData() {
-    count.innerText = getData.length;
-    tbody.innerHTML = "";
-    getData.forEach((el, index) => {
-        let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
-        td1.innerText = el.name;
-        let td2 = document.createElement("td");
-        td2.innerText = el.desc;
-        let td3 = document.createElement("td");
-        td3.innerText = el.date1;
-        let td4 = document.createElement("td");
-        td4.innerText = el.date2;
-        let td5 = document.createElement("td");
-        td5.innerText = el.priority;
-        let td6 = document.createElement("td");
-        td6.innerText = "Add";
-
-td6.addEventListener("click",(elem)=>{
-    
-})
-
-        tr.append(td1, td2, td3, td4, td5, td6);
-        tbody.append(tr);
-
-    })
 
 }
-showData();
+
+
+
+function showUser() {
+
+    task_count.textContent = userData.length;
+    tbody.innerHTML = "";
+    userData.forEach(function(user, index) {
+        let tr = document.createElement("tr")
+        let name = document.createElement("td")
+        let description = document.createElement("td")
+        let startDate = document.createElement("td")
+        let endDate = document.createElement("td")
+        let priority = document.createElement("td")
+        let add = document.createElement("td")
+        name.textContent = user.name;
+        description.textContent = user.description;
+        startDate.textContent = user.startDate
+        endDate.textContent = user.endDate;
+        priority.textContent = user.priority;
+        add.textContent = "Add"
+        add.addEventListener("click", function() {
+            priority_list.push(user)
+            localStorage.setItem("priority-list", JSON.stringify(priority_list))
+            userData.splice(index, 1)
+            localStorage.setItem("task-list", JSON.stringify(userData))
+            showUser()
+        })
+        tr.append(name, description, startDate, endDate, priority, add)
+        tbody.append(tr)
+    })
+
+
+}
+
+showUser()
