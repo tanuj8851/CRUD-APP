4ce79ecbb14d1e4174e7ce7e9854510bc50333fd
tanuj8851 <tanujmaurya40@gmail.com>
2022-12-31 11:56:58 +0530

    Question-1 done

---
+++ unit-3/sprint-2/day-1/assignments/masai-ops-506-165-DUQJdH-bf7a7873ec8b1b02e636a64592c2faccca31c8c9/src/vehicles.js
@@ -0,0 +1,30 @@
+// Function constructor
+function FourWheeler(wheels, model, color, speed, fuel) {
+    this.wheels = wheels;
+    this.model = model;
+    this.color = color;
+    this.speed = speed;
+    this.fuel = fuel;
+    Object.setPrototypeOf(FourWheelerObject, this);
+}
+
+FourWheeler.prototype.setSpeed = function(x) {
+    this.speed = x;
+}
+FourWheeler.prototype.updateColor = function(x) {
+    this.color = x;
+}
+FourWheeler.prototype.updateFuel = function(x) {
+    this.fuel = x;
+}
+
+// Parent object for Object.create
+let FourWheelerObject = {};
+
+// Store function contructor object here
+var car1 = new FourWheeler("wheel", "model", "color", "speed", "fuel");
+
+// Store Object.create here
+var car2 = Object.create(FourWheelerObject);
+
+export { car1, car2 };
