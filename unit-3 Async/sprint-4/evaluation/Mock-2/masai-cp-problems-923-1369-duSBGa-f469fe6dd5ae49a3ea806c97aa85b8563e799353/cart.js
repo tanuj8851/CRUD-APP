d94bbaf76db19b7e5af4be7c55128fb54315e4fc
tanuj8851 <tanujmaurya40@gmail.com>
2023-02-18 20:57:21 +0530

    Mock-2 Done

---
+++ unit-3 Async/sprint-4/evaluation/Mock-2/masai-cp-problems-923-1369-duSBGa-f469fe6dd5ae49a3ea806c97aa85b8563e799353/cart.js
@@ -0,0 +1,61 @@
+let main = document.getElementById("cart-container");
+let getData = JSON.parse(localStorage.getItem("cart"));
+let url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products/";
+let fetchedData = [];
+let total = document.getElementById("cart-total");
+
+window.addEventListener("load", () => {
+    fetchData()
+        // console.log(total.innerText)
+
+})
+
+
+function fetchData() {
+    console.log(getData.length)
+    if (getData.length > 0) {
+        getData.map((el) => {
+            fechingData(el)
+
+        })
+    }
+
+}
+
+function fechingData(id) {
+    fetch(`${url}${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data.data)
+            let fetchedData = data.data;
+            console.log(fetchedData)
+            displayData(fetchedData)
+        })
+}
+
+function displayData(el) {
+    // main.innerHTML = null;
+    // data.forEach(el => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let img = document.createElement("img")
+    img.src = el.image;
+    let title = document.createElement("p");
+    title.innerText = el.title;
+    let brand = document.createElement("p")
+    brand.innerText = el.brand;
+    let category = document.createElement("p");
+    category.innerText = el.category;
+    let price = document.createElement("p");
+    price.innerText = el.price;
+    total.innerText = +(total.innerText) + el.price;
+
+    // console.log(typeof(max))
+
+    card.append(img, title, brand, category, price)
+
+    main.append(card);
+
+
+
+}
---
+++ unit-3 Async/sprint-4/evaluation/Mock-2/masai-cp-problems-923-1369-duSBGa-f469fe6dd5ae49a3ea806c97aa85b8563e799353/index.js
@@ -0,0 +1,103 @@
+let main = document.getElementById("product-container");
+let fetchedData = [];
+let getData = JSON.parse(localStorage.getItem("cart")) || [];
+let alert = document.getElementById("alert");
+let brandfilter = document.getElementById("brand-filter");
+let categoryFilter = document.getElementById("category-filter");
+
+
+brandfilter.addEventListener("change", (e) => {
+    // console.log(brandfilter.value)
+    let Target = e.target;
+    if (Target.checked) {
+        console.log(Target.id)
+        let filteredData = fetchedData.filter((item) => {
+            return Target.id == item.brand;
+
+        })
+        console.log(filteredData)
+        displayData(filteredData)
+    } else {
+        displayData(fetchedData)
+    }
+})
+
+categoryFilter.addEventListener("change", (e) => {
+    let Target = e.target;
+    if (Target.checked) {
+        console.log(Target.id)
+        let filteredData = fetchedData.filter((item) => {
+            return Target.id == item.category;
+
+        })
+        console.log(filteredData)
+        displayData(filteredData)
+    } else {
+        displayData(fetchedData)
+    }
+})
+
+
+
+let url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products";
+
+
+window.addEventListener("load", () => {
+    fetchData();
+})
+
+
+
+function fetchData() {
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+
+            fetchedData = data.data;
+            console.log(fetchedData)
+            displayData(fetchedData)
+        })
+}
+
+
+function displayData(data) {
+    main.innerHTML = null;
+    data.forEach(el => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let img = document.createElement("img")
+        img.src = el.image;
+        let title = document.createElement("p");
+        title.innerText = el.title;
+        let brand = document.createElement("p")
+        brand.innerText = el.brand;
+        let category = document.createElement("p");
+        category.innerText = el.category;
+        let price = document.createElement("p");
+        price.innerText = el.price;
+        let addToCart = document.createElement("button");
+        addToCart.innerText = "Add To Cart";
+
+        addToCart.addEventListener("click", () => {
+
+            if (!getData.includes(el.id)) {
+
+                getData.push(el.id);
+                alert.innerText = "Product added to The cart";
+                localStorage.setItem("cart", JSON.stringify(getData))
+            } else {
+                alert.innerText = "Product already in the cart";
+            }
+
+
+
+
+        })
+
+        card.append(img, title, brand, category, price, addToCart)
+
+        main.append(card);
+
+
+    });
+}
