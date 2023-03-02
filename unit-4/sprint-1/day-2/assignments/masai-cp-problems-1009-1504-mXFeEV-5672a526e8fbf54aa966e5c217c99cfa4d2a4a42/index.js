4d7077f55c1beb4cac498c7bdb0e33247a7249a4
tanuj8851 <tanujmaurya40@gmail.com>
2023-03-02 23:36:23 +0530

    Day-2 done

---
+++ unit-4/sprint-1/day-2/assignments/masai-cp-problems-1009-1504-mXFeEV-5672a526e8fbf54aa966e5c217c99cfa4d2a4a42/index.js
@@ -0,0 +1,18 @@
+// index.js
+let crypto = require("crypto")
+
+//  import the crypto module
+
+
+
+//  get a commands using process.argv
+
+
+// complete the  function
+
+
+
+switch (operation) {
+
+    default: console.log("Invalid operation");
+}
