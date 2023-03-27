7dac0573c24d1b10db8e3e731e25defa8345296a
tanuj8851 <tanujmaurya40@gmail.com>
2023-03-27 10:34:38 +0530

    CRUD APP DONE

--- unit-4/sprint-4/day-2/assignments/CRUD APP/Backend/index.js
+++ unit-4/sprint-4/day-2/assignments/CRUD APP/Backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const swaggerJSdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+
 require('dotenv').config()
 const { userRouter } = require("./routes/user.route")
 const { noteRouter } = require("./routes/note.route")
@@ -10,6 +13,22 @@ const { authenticate } = require("./middlewares/authenticate.middleware")
 
 app.use(express.json());
 app.use(cors())
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Learning Swagger",
+            version: "1.0.0"
+        },
+        servers: [{
+            url: "http://localhost:9000"
+        }]
+    },
+    apis: ["./routes/*.js"]
+}
+const swaggerSpec = swaggerJSdoc(options)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.get("/", (req, res) => {
     res.send("Home Page");
